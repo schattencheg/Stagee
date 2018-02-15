5e4c1586a05e3f9a0725263c55b15bbd276eb826
schattencheg <schattencheg@gmail.com>
2018-02-15 17:18:19 +0300

    Decimal develop

--- Stagee/Service/Decimal.cpp
+++ Stagee/Service/Decimal.cpp
@@ -1,6 +1,19 @@
 #include "Decimal.h"
 
-Decimal::Decimal()
+Decimal::Decimal(double value)
 {
-
+    m_int = floor(value);
+    m_float = (value - m_int) * 1e18;
 }
+
+Decimal::Decimal(long int_part, unsigned long float_part)
+{
+    m_int = int_part;
+    m_float = float_part;
+}
+
+//Decimal::Decimal(Decimal value)
+//{
+//    m_int = value.m_int;
+//    m_float = value.m_float;
+//}
--- Stagee/Service/Decimal.h
+++ Stagee/Service/Decimal.h
@@ -1,11 +1,24 @@
 #ifndef DECIMAL_H
 #define DECIMAL_H
 
+#include <math.h>
 
-class Decimal
-{
-public:
-    Decimal();
+class Decimal {
+ public:
+  Decimal(double value);
+  Decimal(long int int_part, unsigned long int float_part);
+  //Decimal(Decimal value);
+
+  friend const Decimal operator+(const Decimal& left, const Decimal& right) {
+        Decimal newValue(0.0);
+        newValue.m_int = left.m_int + right.m_int + (left.m_float+right.m_float)*1e-18;
+        newValue.m_float = (left.m_float*1e-18 + right.m_float*1e-18)*1e18;
+        return newValue;
+      }
+
+ protected:
+  long int m_int;
+  unsigned long int m_float;
 };
 
-#endif // DECIMAL_H
+#endif  // DECIMAL_H
